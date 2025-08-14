f3c86927c8125156b36e7ab0ff799e422b648b1e
Ali Mirarab <mirarabali@gmail.com>
2025-08-14 22:30:46 +0330

    changes 16

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -79,12 +79,13 @@ export default function Home() {
                                             'text-white bg-gray-100 md:bg-transparent rounded-lg md:rounded-none w-full md:w-auto',
                                             'dark:border-gray-800 dark:bg-white/[0.03] dark:text-gray-400 gap-1 md:gap-0'
                                         )}>
+
                                         <svg
                                             className="size-5 md:size-6 fill-gray-400 dark:fill-gray-200"
                                             xmlns="http://www.w3.org/2000/svg"
-                                            viewBox="0 0 384 512">
+                                             viewBox="0 0 384 512">
                                             <path
-                                                d="M324.5 411.1c6.2 6.2 16.4 6.2 22.6 0s6.2-16.4 0-22.6L214.6 256 347.1 123.5c6.2-6.2 6.2-16.4 0-22.6s-16.4-6.2-22.6 0L192 233.4 59.6 100.9c-6.2-6.2-16.4-6.2-22.6 0s-6.2 16.4 0 22.6L169.4 256 36.9 388.5c-6.2 6.2-6.2 16.4 0 22.6s16.4 6.2 22.6 0L192 278.6 324.5 411.1z"/>
+                                                d="M345 137c9.4-9.4 9.4-24.6 0-33.9s-24.6-9.4-33.9 0l-119 119L73 103c-9.4-9.4-24.6-9.4-33.9 0s-9.4 24.6 0 33.9l119 119L39 375c-9.4 9.4-9.4 24.6 0 33.9s24.6 9.4 33.9 0l119-119L311 409c9.4 9.4 24.6 9.4 33.9 0s9.4-24.6 0-33.9l-119-119L345 137z"/>
                                         </svg>
                                         <span className={'md:hidden text-gray-400 text-sm'}>clear</span>
                                     </button>
