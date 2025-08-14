8f6ed3a498bc364da1ea1f34a7a3cdd265583dbf
Ali Mirarab <mirarabali@gmail.com>
2025-08-14 03:34:13 +0330

    changes 16

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -58,7 +58,7 @@ export default function Home() {
                                     type="text"
                                     placeholder="Here"
                                     className={cn(
-                                        'dark:bg-dark-900 w-full rounded-lg border-none md:border border-gray-300',
+                                        'dark:bg-dark-900 w-full rounded-lg border-none md:border md:border-gray-400',
                                         'bg-white py-3.5 pl-4 pr-4 md:pr-38 text-lg text-gray-800 shadow-none md:shadow-xs',
                                         'placeholder:text-gray-400 focus:border-blue-300 focus:outline-0',
                                         'dark:border-gray-800 dark:bg-gray-900',
@@ -103,134 +103,67 @@ export default function Home() {
                             </div>
                         </form>
 
-                        {/*<div className="rounded-lg flex flex-col w-full overflow-hidden">*/}
-                        {/*    <div*/}
-                        {/*        className="px-4 py-6 bg-white border-b border-slate-200 inline-flex justify-start items-center gap-3">*/}
-                        {/*        <div className="flex-1 flex justify-center items-center gap-3">*/}
-                        {/*            <div className="w-8 h-8 relative overflow-hidden">*/}
-                        {/*                <div*/}
-                        {/*                    className="w-3 h-2.5 left-[5px] top-[8.75px] absolute rounded-tl-3xl rounded-tr-3xl border-[3px] border-sky-500"/>*/}
-                        {/*                <div*/}
-                        {/*                    className="w-4 h-4 left-[10.92px] top-[7px] absolute rounded-lg border-[3px] border-sky-500"/>*/}
-                        {/*                <div*/}
-                        {/*                    className="w-[3px] h-[3px] left-[20.50px] top-[13.50px] absolute bg-sky-500 rounded-full"/>*/}
-                        {/*            </div>*/}
-                        {/*            <div className="flex-1 inline-flex flex-col justify-center items-start gap-2">*/}
-                        {/*                <div className="inline-flex justify-start items-start gap-2">*/}
-                        {/*                    <div*/}
-                        {/*                        className="justify-start text-Color-Primary-Primary text-base font-medium leading-none">amzn.id/ffYHHcGm*/}
-                        {/*                    </div>*/}
-                        {/*                </div>*/}
-                        {/*                <div*/}
-                        {/*                    className="self-stretch justify-start text-slate-500 text-xs font-normal leading-none">amazon.com/dp/B0CSLKLKS/sjikjd=jndd_jdjmLldhsnkos*/}
-                        {/*                </div>*/}
-                        {/*            </div>*/}
-                        {/*        </div>*/}
-                        {/*        <div className="flex justify-start items-center gap-1">*/}
-                        {/*            <div*/}
-                        {/*                className="w-8 h-8 p-2 bg-Color-Light-Light rounded-md flex justify-center items-center gap-2.5">*/}
-                        {/*                <div className="w-4 h-4 relative overflow-hidden"/>*/}
-                        {/*            </div>*/}
-                        {/*            <div*/}
-                        {/*                className="w-8 h-8 p-2 bg-Color-Light-Light rounded-md flex justify-center items-center gap-2.5">*/}
-                        {/*                <div className="w-4 h-4 relative overflow-hidden">*/}
-                        {/*                    <div*/}
-                        {/*                        className="w-3 h-3 left-[0.82px] top-[5.44px] absolute opacity-10 bg-Color-Grey-Grey-500"/>*/}
-                        {/*                    <div*/}
-                        {/*                        className="w-4 h-4 left-[0.75px] top-[0.75px] absolute bg-Color-Grey-Grey-500"/>*/}
-                        {/*                </div>*/}
-                        {/*            </div>*/}
-                        {/*            <div data-size="Medium" data-state="Default" data-style="Solid"*/}
-                        {/*                 className="w-8 h-8 p-2 bg-Color-Light-Light rounded-md flex justify-center items-center gap-2.5">*/}
-                        {/*                <div className="w-4 h-4 relative overflow-hidden"/>*/}
-                        {/*            </div>*/}
-                        {/*        </div>*/}
-                        {/*    </div>*/}
-                        {/*    <div*/}
-                        {/*        className="px-4 py-6 bg-b&w-white border-b border-slate-200 inline-flex justify-start items-center gap-3">*/}
-                        {/*        <div className="flex-1 flex justify-center items-center gap-3">*/}
-                        {/*            <div className="w-8 h-8 relative overflow-hidden">*/}
-                        {/*                <div*/}
-                        {/*                    className="w-3 h-2.5 left-[5px] top-[8.75px] absolute rounded-tl-3xl rounded-tr-3xl border-[3px] border-sky-500"/>*/}
-                        {/*                <div*/}
-                        {/*                    className="w-4 h-4 left-[10.92px] top-[7px] absolute rounded-lg border-[3px] border-sky-500"/>*/}
-                        {/*                <div*/}
-                        {/*                    className="w-[3px] h-[3px] left-[20.50px] top-[13.50px] absolute bg-sky-500 rounded-full"/>*/}
-                        {/*            </div>*/}
-                        {/*            <div className="flex-1 inline-flex flex-col justify-center items-start gap-2">*/}
-                        {/*                <div className="inline-flex justify-start items-start gap-2">*/}
-                        {/*                    <div*/}
-                        {/*                        className="justify-start text-Color-Primary-Primary text-base font-medium font-['Inter'] leading-none">amzn.id/ffYHHcGm*/}
-                        {/*                    </div>*/}
-                        {/*                </div>*/}
-                        {/*                <div*/}
-                        {/*                    className="self-stretch justify-start text-slate-500 text-xs font-normal font-['Inter'] leading-none">amazon.com/dp/B0CSLKLKS/sjikjd=jndd_jdjmLldhsnkos*/}
-                        {/*                </div>*/}
-                        {/*            </div>*/}
-                        {/*        </div>*/}
-                        {/*        <div className="flex justify-start items-center gap-1">*/}
-                        {/*            <div*/}
-                        {/*                className="w-8 h-8 p-2 bg-Color-Light-Light rounded-md flex justify-center items-center gap-2.5">*/}
-                        {/*                <div className="w-4 h-4 relative overflow-hidden"/>*/}
-                        {/*            </div>*/}
-                        {/*            <div*/}
-                        {/*                className="w-8 h-8 p-2 bg-Color-Light-Light rounded-md flex justify-center items-center gap-2.5">*/}
-                        {/*                <div className="w-4 h-4 relative overflow-hidden">*/}
-                        {/*                    <div*/}
-                        {/*                        className="w-3 h-3 left-[0.82px] top-[5.44px] absolute opacity-10 bg-Color-Grey-Grey-500"/>*/}
-                        {/*                    <div*/}
-                        {/*                        className="w-4 h-4 left-[0.75px] top-[0.75px] absolute bg-Color-Grey-Grey-500"/>*/}
-                        {/*                </div>*/}
-                        {/*            </div>*/}
-                        {/*            <div data-size="Medium" data-state="Default" data-style="Solid"*/}
-                        {/*                 className="w-8 h-8 p-2 bg-Color-Light-Light rounded-md flex justify-center items-center gap-2.5">*/}
-                        {/*                <div className="w-4 h-4 relative overflow-hidden"/>*/}
-                        {/*            </div>*/}
-                        {/*        </div>*/}
-                        {/*    </div>*/}
-                        {/*    <div*/}
-                        {/*        className="px-4 py-6 bg-b&w-white border-b inline-flex justify-start items-center gap-3 overflow-hidden">*/}
-                        {/*        <div className="flex-1 flex justify-start items-center gap-3">*/}
-                        {/*            <div className="w-8 h-8 relative overflow-hidden">*/}
-                        {/*                <div*/}
-                        {/*                    className="w-3 h-2.5 left-[5px] top-[8.75px] absolute rounded-tl-3xl rounded-tr-3xl border-[3px] border-sky-500"/>*/}
-                        {/*                <div*/}
-                        {/*                    className="w-4 h-4 left-[10.92px] top-[7px] absolute rounded-lg border-[3px] border-sky-500"/>*/}
-                        {/*                <div*/}
-                        {/*                    className="w-[3px] h-[3px] left-[20.50px] top-[13.50px] absolute bg-sky-500 rounded-full"/>*/}
-                        {/*            </div>*/}
-                        {/*            <div className="flex-1 inline-flex flex-col justify-center items-start gap-2">*/}
-                        {/*                <div className="inline-flex justify-start items-start gap-2">*/}
-                        {/*                    <div*/}
-                        {/*                        className="justify-start text-Color-Primary-Primary text-base font-medium font-['Inter'] leading-none">amzn.id/ffYHHcGm*/}
-                        {/*                    </div>*/}
-                        {/*                </div>*/}
-                        {/*                <div*/}
-                        {/*                    className="justify-start text-slate-500 text-xs font-normal font-['Inter'] leading-none">amazon.com/dp/B0CSLKLKS/sjikjd=jndd_jdjmLldhsnkos*/}
-                        {/*                </div>*/}
-                        {/*            </div>*/}
-                        {/*        </div>*/}
-                        {/*        <div className="flex justify-start items-center gap-1">*/}
-                        {/*            <div*/}
-                        {/*                className="w-8 h-8 p-2 bg-Color-Light-Light rounded-md flex justify-center items-center gap-2.5">*/}
-                        {/*                <div className="w-4 h-4 relative overflow-hidden"/>*/}
-                        {/*            </div>*/}
-                        {/*            <div*/}
-                        {/*                className="w-8 h-8 p-2 bg-Color-Light-Light rounded-md flex justify-center items-center gap-2.5">*/}
-                        {/*                <div className="w-4 h-4 relative overflow-hidden">*/}
-                        {/*                    <div*/}
-                        {/*                        className="w-3 h-3 left-[0.82px] top-[5.44px] absolute opacity-10 bg-Color-Grey-Grey-500"/>*/}
-                        {/*                    <div*/}
-                        {/*                        className="w-4 h-4 left-[0.75px] top-[0.75px] absolute bg-Color-Grey-Grey-500"/>*/}
-                        {/*                </div>*/}
-                        {/*            </div>*/}
-                        {/*            <div data-size="Medium" data-state="Default" data-style="Solid"*/}
-                        {/*                 className="w-8 h-8 p-2 bg-Color-Light-Light rounded-md flex justify-center items-center gap-2.5">*/}
-                        {/*                <div className="w-4 h-4 relative overflow-hidden"/>*/}
-                        {/*            </div>*/}
-                        {/*        </div>*/}
-                        {/*    </div>*/}
-                        {/*</div>*/}
+                        <div className="rounded-lg flex flex-col w-full overflow-hidden">
+                            <div
+                                className="px-4 py-6 bg-white last:border-none border-b border-gray-200 flex items-center justify-between flex-wrap gap-3">
+                                <div className="flex justify-center items-center gap-3">
+                                    <div className="relative overflow-hidden">
+                                        <svg className={'size-8 fill-none'} viewBox="0 0 48 48"
+                                             xmlns="http://www.w3.org/2000/svg">
+                                            <path
+                                                d="M25.5 27.75V22.5C25.5 17.5294 21.4706 13.5 16.5 13.5V13.5C11.5294 13.5 7.5 17.5294 7.5 22.5L7.5 27.75"
+                                                stroke="#0094FF" strokeWidth="4.5" strokeLinecap="round"/>
+                                            <path
+                                                d="M16.3865 22.875V25.5C16.3865 32.1274 21.7591 37.5 28.3865 37.5V37.5C35.014 37.5 40.3865 32.1274 40.3865 25.5V21.375C40.3865 15.576 35.6855 10.875 29.8865 10.875V10.875"
+                                                stroke="#0094FF" strokeWidth="4.5" strokeLinecap="round"/>
+                                            <circle cx="33" cy="22.875" r="2.25" fill="#0094FF"/>
+                                        </svg>
+                                    </div>
+                                    <div className="flex flex-col justify-center items-start gap-2">
+                                        <div className="inline-flex justify-start items-start gap-2">
+                                            <div
+                                                className="justify-start text-Color-Primary-Primary text-base font-medium leading-none">amzn.id/ffYHHcGm
+                                            </div>
+                                        </div>
+                                        <div
+                                            className="self-stretch justify-start text-slate-500 text-xs font-normal leading-none">amazon.com/dp/B0CSLKLKS/sjikjd=jndd_jdjmLldhsnkos
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="flex justify-start items-center gap-1">
+                                    <div
+                                        className="p-2 rounded-lg flex justify-center items-center gap-2.5">
+                                        <svg
+                                            className={'size-5 fill-gray-400'}
+                                            xmlns="http://www.w3.org/2000/svg"
+                                             viewBox="0 0 448 512">
+                                            <path
+                                                d="M144 64L48 64c-8.8 0-16 7.2-16 16l0 96c0 8.8 7.2 16 16 16l96 0c8.8 0 16-7.2 16-16l0-96c0-8.8-7.2-16-16-16zM48 32l96 0c26.5 0 48 21.5 48 48l0 96c0 26.5-21.5 48-48 48l-96 0c-26.5 0-48-21.5-48-48L0 80C0 53.5 21.5 32 48 32zm96 288l-96 0c-8.8 0-16 7.2-16 16l0 96c0 8.8 7.2 16 16 16l96 0c8.8 0 16-7.2 16-16l0-96c0-8.8-7.2-16-16-16zM48 288l96 0c26.5 0 48 21.5 48 48l0 96c0 26.5-21.5 48-48 48l-96 0c-26.5 0-48-21.5-48-48l0-96c0-26.5 21.5-48 48-48zM304 64c-8.8 0-16 7.2-16 16l0 96c0 8.8 7.2 16 16 16l96 0c8.8 0 16-7.2 16-16l0-96c0-8.8-7.2-16-16-16l-96 0zM256 80c0-26.5 21.5-48 48-48l96 0c26.5 0 48 21.5 48 48l0 96c0 26.5-21.5 48-48 48l-96 0c-26.5 0-48-21.5-48-48l0-96zm0 224c0-8.8 7.2-16 16-16l64 0c8.8 0 16 7.2 16 16l0 68 64 0 0-68c0-8.8 7.2-16 16-16s16 7.2 16 16l0 84c0 8.8-7.2 16-16 16l-96 0c-8.8 0-16-7.2-16-16l0-68-32 0 0 152c0 8.8-7.2 16-16 16s-16-7.2-16-16l0-168zM88 104l16 0c8.8 0 16 7.2 16 16l0 16c0 8.8-7.2 16-16 16l-16 0c-8.8 0-16-7.2-16-16l0-16c0-8.8 7.2-16 16-16zM72 376c0-8.8 7.2-16 16-16l16 0c8.8 0 16 7.2 16 16l0 16c0 8.8-7.2 16-16 16l-16 0c-8.8 0-16-7.2-16-16l0-16zM344 104l16 0c8.8 0 16 7.2 16 16l0 16c0 8.8-7.2 16-16 16l-16 0c-8.8 0-16-7.2-16-16l0-16c0-8.8 7.2-16 16-16zM320 448c0-8.8 7.2-16 16-16l16 0c8.8 0 16 7.2 16 16l0 16c0 8.8-7.2 16-16 16l-16 0c-8.8 0-16-7.2-16-16l0-16zm96-16l16 0c8.8 0 16 7.2 16 16l0 16c0 8.8-7.2 16-16 16l-16 0c-8.8 0-16-7.2-16-16l0-16c0-8.8 7.2-16 16-16z"/>
+                                        </svg>
+                                    </div>
+                                    <button type={'button'}
+                                            className="p-2 rounded-lg flex justify-center items-center gap-2.5">
+                                        <svg
+                                            className={'size-5 fill-gray-400 rotate-180'}
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            viewBox="0 0 512 512">
+                                            <path
+                                                d="M64 480l224 0c17.7 0 32-14.3 32-32l0-64 32 0 0 64c0 35.3-28.7 64-64 64L64 512c-35.3 0-64-28.7-64-64L0 224c0-35.3 28.7-64 64-64l64 0 0 32-64 0c-17.7 0-32 14.3-32 32l0 224c0 17.7 14.3 32 32 32zM224 320l224 0c17.7 0 32-14.3 32-32l0-224c0-17.7-14.3-32-32-32L224 32c-17.7 0-32 14.3-32 32l0 224c0 17.7 14.3 32 32 32zm-64-32l0-224c0-35.3 28.7-64 64-64L448 0c35.3 0 64 28.7 64 64l0 224c0 35.3-28.7 64-64 64l-224 0c-35.3 0-64-28.7-64-64z"/>
+                                        </svg>
+                                    </button>
+                                    <button type={'button'}
+                                            className="p-2 rounded-lg flex justify-center items-center gap-2.5">
+                                        <svg
+                                            className={'size-5 fill-gray-400'}
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            viewBox="0 0 128 512">
+                                            <path
+                                                d="M64 368a48 48 0 1 0 0 96 48 48 0 1 0 0-96zm0-160a48 48 0 1 0 0 96 48 48 0 1 0 0-96zM112 96A48 48 0 1 0 16 96a48 48 0 1 0 96 0z"/>
+                                        </svg>
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
 
                     </div>
                 </div>
