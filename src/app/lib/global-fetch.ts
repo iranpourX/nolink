4fa09555fde47a00f07fe27fb15a5afc267c9117
Ali Mirarab <mirarabali@gmail.com>
2025-08-18 02:00:55 +0330

    changes 17

--- src/app/lib/global-fetch.ts
+++ src/app/lib/global-fetch.ts
@@ -1,15 +1,22 @@
-const originalFetch = global.fetch
+// lib/global-fetch.ts
+
+let currentUserAgent: string | null = null;
+
+// تابع برای ست کردن user-agent از request اصلی
+export function setUserAgent(ua: string) {
+    currentUserAgent = ua;
+}
+
+const originalFetch = global.fetch;
 
 global.fetch = async (input: RequestInfo | URL, init?: RequestInit) => {
     const defaultHeaders: HeadersInit = {
-        "User-Agent": "MyNextApp/1.0.0",
-    }
+        ...(init?.headers || {}),
+        ...(currentUserAgent ? {"User-Agent": currentUserAgent} : {}),
+    };
 
     return originalFetch(input, {
         ...init,
-        headers: {
-            ...defaultHeaders,
-            ...(init?.headers || {})
-        },
-    })
-}
+        headers: defaultHeaders,
+    });
+};
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,12 @@
-import {NextResponse} from "next/server";
+// import {NextResponse} from "next/server";
 import type {NextRequest} from "next/server";
+import {setUserAgent} from "@/app/lib/global-fetch";
 
 export function middleware(request: NextRequest) {
-    const themePreference = request.cookies.get('token')
+    const userAgent = request.headers.get("user-agent") || "Unknown-UA";
+    setUserAgent(userAgent);
+
+    // const themePreference = request.cookies.get('token')
     // if (!themePreference) {
     //     if (request.nextUrl.pathname.startsWith('/panel')) {
     //         return NextResponse.redirect(new URL('/signin', request.url))
